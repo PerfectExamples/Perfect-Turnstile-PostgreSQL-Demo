631920a1d66e862338d9f69b03a27fb37887e330
Jonathan Guthrie <jono@perfect.org>
2017-01-13 14:08:42 -0500

    update logging dep

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-12-08 17:47:11 +0000
+// Date: 2017-01-13 19:06:59 +0000
 import PackageDescription
 let package = Package(
     name: "PerfectTurnstilePostgreSQLDemo",
     targets: [],
     dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Turnstile-PostgreSQL.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 1, minor: 0),
     ]
 )
